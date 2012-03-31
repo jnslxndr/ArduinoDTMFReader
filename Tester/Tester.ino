e64c12861b47be1ab43761cc056cffa5bdefce27
jens-a-e <jens@ififelse.net>
2012-03-31 16:56:36 +0200

    Using new read implementation in Tester - proved working :)

--- Tester/Tester.ino
+++ Tester/Tester.ino
@@ -13,14 +13,20 @@ void setup()
 {
   softSerial.begin(19200);               // the GPRS baud rate   
   Serial.begin(19200);                 // the GPRS baud rate   
+  
+  setup_dtmf_reader();
 }
 
 void loop()
 {
+  if (dtmf_available()) {
+    Serial.print("DTMF: ");
+    Serial.println(dtmf_read());
+  }
   if (softSerial.available())
     Serial.write(softSerial.read());
   if (Serial.available())
-    softSerial.write(Serial.read());  
+    softSerial.write(Serial.read());
 }
 
 
